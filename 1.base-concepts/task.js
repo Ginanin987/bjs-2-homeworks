89bfcd5392abf76314b450389a37f2319861485a
Sergey Bykov <derter@kitendo.ru>
2023-06-17 11:45:22 +0400

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr = []
+  let d = b ** 2 - 4 * a * c
+  if (d == 0) {
+    arr.push(-b / (2 * a))
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a))
+  }
+  return arr
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] == "number") {
+      continue
+    } else if (typeof arguments[i] == "string") {
+      arguments[i] = Number(arguments[i])
+    } else {
+      return false
+    }
+  }
+  let P = percent / 100 / 12
+  let S = amount - contribution
+  let countPerMonth = S * (P + P / ((1 + P) ** countMonths - 1))
+  let countTotal = (countPerMonth * countMonths).toFixed(2)
+  return Number(countTotal)
 }
